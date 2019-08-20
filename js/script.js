1a33ec932b4b03a2923d48d675be7a941a06aad7
pete88 <peteakhurst@gmail.com>
2019-08-21 09:22:42 +1000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -18,12 +18,7 @@ project 1 - A Random Quote Generator
 
 
 /*** 
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-  Add the `citation` property to at least one object in the array.
-  Add the `year` property to at least one object in the array.
-  Use console.log() to log your array of quotes to the console.
+ Array of quotes
 ***/
 
 let quotes = [
@@ -69,9 +64,7 @@ console.log(quotes);
 
 
 /***
-  Create the `getRandomQuote` function to:
-   - Create a variable to store a random number 
-   - Cse the random number to `return` a random quote object from the `quotes` array.
+ Get random quote function to choose between the 5 quotes to pass to the print quote function
 ***/
 
 function getRandomQuote() {
@@ -86,16 +79,7 @@ console.log(getRandomQuote());
 
 
 /***
-  Create the `printQuote` function to: 
-   - Call the `getRandomQuote` function and assign it to a variable.
-   - Create a variable for the HTML string and set it equal to an empty string.
-   - Use the HTML template in the instructions or the markup in the index.html file, AND 
-     the random quote variable to build your HTML string.
-   - Add the quote and source section to the HTML string.
-   - Use an if statement to check for the citation property before adding it to the HTML string.
-   - Use an if statement to check for the year property before adding it to the HTML string.
-   - Don't forget to close that final `p` tag.
-   - Set the `innerHTML` of the `quote-box` div to the HTML string. 
+ Print quote function to print the randomQuote
 ***/
 
 function printQuote() {
@@ -104,7 +88,7 @@ function printQuote() {
   let HTML = " "; 
 
   
-  HTML += "<p class='quote'> " + randomQuote.quote;
+  HTML += "<p class='quote'> " + randomQuote.quote + "</p>";
   HTML += "<p class='source'> " + randomQuote.source;
   
 
@@ -156,13 +140,7 @@ function randomBackgroundColor() {
 }
 
 /***
-  When the "Show another quote" button is clicked, the event listener 
-  below will be triggered, and it will call, or "invoke", the `printQuote` 
-  function. So do not make any changes to the line of code below this 
-  comment.
+  Button to printQuote when clicked 
 ***/
 
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
